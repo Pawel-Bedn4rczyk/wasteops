87ba73d82dc91913e19b08b8c8f4317f3b11f52a
Paweł Bednarczyk <kontakt@pawelbednarczyk.com>
2026-08-13 01:25:09 +0200

    style: switch to Lato with latin-ext font

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
+                bunny('Lato', {
+                    weights: [400, 700],
+                    subsets: ['latin', 'latin-ext'],
                 }),
             ],
         }),
