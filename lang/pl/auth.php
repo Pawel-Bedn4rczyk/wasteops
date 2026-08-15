8176bc50ccffc17e0219572ce1b475c4e480fc82
Paweł Bednarczyk <kontakt@pawelbednarczyk.com>
2026-08-15 15:44:09 +0200

    feat: translate login auth messages to Polish

---
+++ lang/pl/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+  'failed' => 'Podany email lub hasło jest nieprawidłowe.',
+  'password' => 'Podane hasło jest nieprawidłowe.',
+  'throttle' => 'Zbyt wiele prób logowania. Spróbuj ponownie za :seconds sekund.',
+
+];
